2af47051a1dfb087670ad174bbd87634c22760ba
Jonathan Jewett <jonathanjewett0@gmail.com>
2024-02-29 12:45:38 -0800

    Finished Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return 'ERROR';
+  }
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
+  let sum = 0;
+  let x = num1;
+  let y = num2;
 
+  if (x < y) {
+    for (let i = x; i <= y; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = x; i >= y; i--) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
