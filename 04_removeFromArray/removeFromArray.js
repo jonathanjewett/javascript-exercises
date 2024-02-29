981aba6048d08e41a0250c5b85780f32082a0db1
Jonathan Jewett <jonathanjewett0@gmail.com>
2024-02-28 22:06:47 -0800

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemoved) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!toBeRemoved.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
 
+  return newArray;
 };
 
 // Do not edit below this line
