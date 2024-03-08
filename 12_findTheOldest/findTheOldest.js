098df8dd6553f81b4170fc4479565510ea4d9716
Jonathan Jewett <jonathanjewett0@gmail.com>
2024-03-07 23:29:04 -0800

    finished all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeople) {
+  let age = 0;
+  let oldestPerson = {};
+  for (let i = 0; i < arrayOfPeople.length; i++) {
+    if (!arrayOfPeople[i].yearOfDeath) {
+      if (2024 - arrayOfPeople[i].yearOfBirth > age) {
+        age = 2024 - arrayOfPeople[i].yearOfBirth
+        oldestPerson = arrayOfPeople[i];
+      }
+    } else {
+      if (arrayOfPeople[i].yearOfDeath - arrayOfPeople[i].yearOfBirth > age) {
+        age = arrayOfPeople[i].yearOfDeath - arrayOfPeople[i].yearOfBirth;
+        oldestPerson = arrayOfPeople[i];
+      }
+    }
+    // console.log(age, oldestPerson);
+  }
+  // console.log(oldestPerson);
+  return oldestPerson;
 };
 
 // Do not edit below this line
