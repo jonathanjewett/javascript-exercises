8c22509be5cff00180702add6cec1b854495be8f
Jonathan Jewett <jonathanjewett0@gmail.com>
2024-03-07 22:18:07 -0800

    finished palindromes, still not sure why regex doesnt work using !== though

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // let newString = '';
+  // let reversedString = '';
+  // for (let i = 0; i < string.length; i++) {
+  //   if (string[i] !== /[^a-z0-9]/) {
+  //     newString += string[i];
+  //   }
+  // }
+  // console.log('/[^a-z0-9]/g');
 
+  // for (let i = newString.length - 1; i >= 0; i--) {
+  //   reversedString += newString[i];
+  // }
+
+  // console.log(reversedString);
+
+  // for (let i = 0; i < newString.length; i++) {
+  //   if (newString[i] !== reversedString[i]) {
+  //     return false;
+  //   }
+  // }
+
+  // return true;
+  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return processedString.split("").reverse().join("") === processedString;
 };
 
 // Do not edit below this line
