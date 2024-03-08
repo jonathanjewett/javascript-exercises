03809651c781ae586a2bbaa673df78954ff6c7e6
Jonathan Jewett <jonathanjewett0@gmail.com>
2024-03-07 18:17:45 -0800

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce ((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  if (y === 0) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = 0; i < y; i++) {
+    result = result * x;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  // n! = n * (n - 1) * (n - 2) * (n - 3)...
+  // n! = n * (n - 1)!
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+
+  return n * (factorial(n - 1));
 };
 
 // Do not edit below this line
